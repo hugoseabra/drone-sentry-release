6ec58bab6c74d847acd040b2ea487cc03cd08180
Darren Crossley <darren@studiothirty.io>
2017-11-09 16:46:40 +0100

    Fixing for env vars present in CI

--- script.sh
+++ script.sh
@@ -1,17 +1,17 @@
 # Create a new release
 
 echo 'Updating Sentry Project: '${PLUGIN_SENTRY_PROJECT}
-echo 'New Release: '${DRONE_REPO_NAME}':'${DRONE_COMMIT_SHA}
+echo 'New Release: studiothirty/'${DRONE_REPO_NAME}':'${DRONE_COMMIT_SHA}
 
 curl https://sentry.studiothirty.io/api/0/organizations/studiothirty/releases/ \
   -X POST \
-  -H 'Authorization: Bearer '${PLUGIN_SENTRY_TOKEN} \
+  -H 'Authorization: Bearer '${SENTRY_TOKEN} \
   -H 'Content-Type: application/json' \
   -d '
   {
     "version": "'${DRONE_COMMIT_SHA}'",
     "refs": [{
-        "repository":"'${DRONE_REPO_NAME}'",
+        "repository":"studiothirty/'${DRONE_REPO_NAME}'",
         "commit":"'${DRONE_COMMIT_SHA}'"
     }],
     "projects":["'${PLUGIN_SENTRY_PROJECT}'"]
